b5222697430814a7f84d405d0bccb5487175d54b
midrare <midrare9@gmail.com>
2025-12-02 17:50:38 -0800

    fix: Retry on wallpapers that do not load

--- dot_config/hypr/executable_hyprpaper.sh
+++ dot_config/hypr/executable_hyprpaper.sh
@@ -1,11 +1,46 @@
 #!/usr/bin/env bash
 
-while ! hyprctl version &>/dev/null; do
-    sleep 0.05
+WALLPAPER_DIR="$HOME/.local/share/wallpapers"
+
+systemctl --user start hyprpaper
+until systemctl --user is-active --quiet hyprpaper; do
+    sleep 0.1
 done
 
-WALLPAPER_DIR="$HOME/.local/share/wallpapers"
-CURRENT_WALL="$(hyprctl hyprpaper listloaded)"
-WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
+until hyprctl version &>/dev/null; do
+    sleep 0.1
+done
 
-hyprctl hyprpaper reload ",${WALLPAPER}" >/dev/null
+until hyprctl hyprpaper listloaded &>/dev/null; do
+    sleep 0.1
+done
+
+until pgrep -x hyprpaper &>/dev/null; do
+    sleep 0.1
+done
+
+old_walls="$(hyprctl hyprpaper listloaded | rev | cut -d/ -f1 | rev)"
+all_walls="$(find "$WALLPAPER_DIR" -type f | rev | cut -d/ -f1 | rev)"
+new_wall="$1"
+
+
+# retry because hyprpaper does not load certain image files for some reason
+while true; do
+    if [ -z "$new_wall" ]; then
+        new_wall="$(printf "$old_walls\n$all_walls" | sort -h | uniq -u | shuf -n 1)"
+        if [ -z "$new_wall" ]; then
+            new_wall="$(printf "$old_walls" | head -n 1)"
+        fi
+    fi
+
+    echo "$new_wall"
+
+    hyprctl hyprpaper reload ",$WALLPAPER_DIR/$new_wall" &>/dev/null
+    sleep 1
+
+    if hyprctl hyprpaper listloaded | grep -x "$WALLPAPER_DIR/$new_wall" &>/dev/null; then
+        break
+    fi
+
+    new_wall=""
+done
